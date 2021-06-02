0955ca8a6f8fbfda08a7b241ff90536c34a69a1e
andre ferrer <andrerferrer@hotmail.com>
2021-06-02 10:43:35 -0300

    Add validations to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,17 @@
 class Category < ApplicationRecord
+  PAIRS = {
+    environment: ['sun exposure', 'sleep'],
+    health: ['workout', 'running']
+  }
+
+  validate :pairs_do_match
+
+  def pairs_do_match
+
+    unless PAIRS[self.group.to_sym].include?(self.title)
+      errors.add(:group, "group and title pair don't match")
+      errors.add(:title, "group and title pair don't match")
+    end
+  end
+
 end
