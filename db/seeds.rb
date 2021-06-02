d42632f50ff9654152dc0b10e47a478fbaf2fa9c
andre ferrer <andrerferrer@hotmail.com>
2021-06-02 10:43:42 -0300

    Add seeds for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+PAIRS = {
+    environment: ['sun exposure', 'sleep'],
+    health: ['workout', 'running']
+}
+
+PAIRS.each do |group_name, titles|
+    titles.each do |title|
+        Category.create!(
+            group: group_name.to_s,
+            title: title
+        )
+    end
+end
+
+# The above should create all possible combinations
